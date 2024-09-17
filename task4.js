659953f689c3a989100d872300d37b48ac98dc80
Sumaiya215 <sumaiyacse15>
2024-09-17 01:18:57 -0700

    object homework done

---
+++ task4.js
@@ -0,0 +1,15 @@
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+// let count = 0;
+// for(const properties in student){
+//     count++;
+// }
+// console.log(count);
+
+let count = Object.keys(student).length
+
+console.log(count)
---
+++ task5.js
@@ -0,0 +1,15 @@
+let myObject = {
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+for(const key in myObject){
+    
+        console.log("Key: " + key + " | Type: " + typeof myObject[key]);
+    
+}
+
+
+  
